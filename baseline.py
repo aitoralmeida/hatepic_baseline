ff89e033a48f2445b668792c9166f466584801b5
Aitor Almeida <aitoralmeida@gmail.com>
2018-10-04 14:59:58 +0200

    complete resnet50 baseline

--- baseline.py
+++ baseline.py
@@ -5,19 +5,26 @@ Created on Tue Oct  2 10:06:05 2018
 @author: aitor
 """
 from random import shuffle
+import sys
 import os
 
 from keras.applications.resnet50 import ResNet50
 from keras.layers import Dense
 from keras.models import Model
 from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.resnet50 import preprocess_input
 import numpy as np
+from sklearn import metrics
 
 TRAIN_PATH = "./dataset/train/"
 VAL_PATH = "./dataset/val/"
 TEST_PATH = "./dataset/test/"
 
+'''
+********************************************************
+*******************RESNET50*****************************
+********************************************************
+'''
 def build_resnet50_model():
     # get the model without the denses
     base_model = ResNet50(weights='imagenet', include_top='false')
@@ -75,23 +82,66 @@ def get_training_data_resnet50():
         X_val.append(x)
     print 'Total val examples: ' + str(len(y_val))
     print 'No hate examples: ' + str(total_no_hate)
-    print 'Hate examples: ' + str(total_hate)
-    
+    print 'Hate examples: ' + str(total_hate)  
     
     X_train = np.array(X_train)
     y_train = np.array(y_train)
     X_val = np.array(X_val)
     y_val = np.array(y_val)
-
     return X_train, y_train, X_val, y_val
+
+def get_test_data_resnet50(): 
+    print '*****TEST*****'
+    filenames = os.listdir(TEST_PATH) 
+    shuffle(filenames)
+    X_test = []
+    y_test = []
+    total_hate = 0
+    total_no_hate = 0
+    for filename in filenames:
+        if "no_hate" in filename:
+            y_test.append(0)
+            total_no_hate += 1
+        else:
+            y_test.append(1)
+            total_hate += 1
+        img = image.load_img(TEST_PATH+filename, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = preprocess_input(x)
+        X_test.append(x)
+    print 'Total val examples: ' + str(len(y_test))
+    print 'No hate examples: ' + str(total_no_hate)
+    print 'Hate examples: ' + str(total_hate)  
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+    return X_test, y_test
+
+def calculate_evaluation_metrics(y_gt, y_preds):        
+    metric_types =  ['micro', 'macro', 'weighted']
+    metric_results = {
+        'precision' : {},
+        'recall' : {},
+        'f1' : {},
+        'acc' : -1.0        
+    }
+            
+    for t in metric_types:
+        metric_results['precision'][t] = metrics.precision_score(y_gt, y_preds, average = t)
+        metric_results['recall'][t] = metrics.recall_score(y_gt, y_preds, average = t)
+        metric_results['f1'][t] = metrics.f1_score(y_gt, y_preds, average = t)
+        metric_results['acc'] = metrics.accuracy_score(y_gt, y_preds) 
+                
+    return metric_results
 
 if __name__ == "__main__":
     print 'Starting'
     print 'Building model...'
+    sys.stdout.flush()
     model = build_resnet50_model()
     model.summary()
     print 'Building data...'
     X_train, y_train, X_val, y_val = get_training_data_resnet50()
+    sys.stdout.flush()
     print 'X_train: ' + str(len(X_train))
     print(X_train.shape)
     print 'y_train: ' + str(len(y_train))
@@ -101,5 +151,26 @@ if __name__ == "__main__":
     print 'y_val: ' + str(len(y_val))
     print(y_val.shape)
     print 'Training mode...'
+    sys.stdout.flush()
     results = model.fit(X_train, y_train, epochs= 100, batch_size = 32, validation_data = (X_val, y_val))
+    print 'Evaluating last model...'
+    sys.stdout.flush()
+    X_test, y_test = get_test_data_resnet50()
+    yp = model.predict(X_test, batch_size=32, verbose=1)
+    y_preds = np.argmax(yp, axis=1)
+    print '*'*30
+    print 'PREDS'
+    sys.stdout.flush()
+    print y_preds
+    sys.stdout.flush()
+    print '*'*30
+    print 'TEST'
+    sys.stdout.flush()
+    print y_test
+    sys.stdout.flush()
+    evaluation = calculate_evaluation_metrics(y_test, y_preds)
+    print 'accuracy: ', evaluation['acc']
+    print 'precision:', evaluation['precision']
+    print 'recall:', evaluation['recall']
+    print 'f1:', evaluation['f1'] 
     print 'fin'
