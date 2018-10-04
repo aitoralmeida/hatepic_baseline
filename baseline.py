60bf7118a4321404821f3e549f3c05e71af3562a
Aitor Almeida <aitoralmeida@gmail.com>
2018-10-04 16:13:52 +0200

    added xception baseline

--- baseline.py
+++ baseline.py
@@ -8,11 +8,13 @@ from random import shuffle
 import sys
 import os
 
-from keras.applications.resnet50 import ResNet50
+#from keras.applications.resnet50 import ResNet50
+import keras.applications.resnet50 as resnet50
+import keras.applications.xception as xception
 from keras.layers import Dense
 from keras.models import Model
 from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input
+#from keras.applications.resnet50 import preprocess_input
 import numpy as np
 from sklearn import metrics
 
@@ -27,7 +29,7 @@ TEST_PATH = "./dataset/test/"
 '''
 def build_resnet50_model():
     # get the model without the denses
-    base_model = ResNet50(weights='imagenet', include_top='false')
+    base_model = resnet50.ResNet50(weights='imagenet', include_top='false')
     new_dense = base_model.output
     # add the new denses to classify the hate images
     new_dense = Dense(1024, activation='relu')(new_dense)
@@ -56,7 +58,7 @@ def get_training_data_resnet50():
             total_hate += 1
         img = image.load_img(TRAIN_PATH+filename, target_size=(224, 224))
         x = image.img_to_array(img)
-        x = preprocess_input(x)
+        x = resnet50.preprocess_input(x)
         X_train.append(x)
     print 'Total train examples: ' + str(len(y_train))
     print 'No hate examples: ' + str(total_no_hate)
@@ -78,7 +80,7 @@ def get_training_data_resnet50():
             total_hate += 1
         img = image.load_img(VAL_PATH+filename, target_size=(224, 224))
         x = image.img_to_array(img)
-        x = preprocess_input(x)
+        x = resnet50.preprocess_input(x)
         X_val.append(x)
     print 'Total val examples: ' + str(len(y_val))
     print 'No hate examples: ' + str(total_no_hate)
@@ -107,7 +109,7 @@ def get_test_data_resnet50():
             total_hate += 1
         img = image.load_img(TEST_PATH+filename, target_size=(224, 224))
         x = image.img_to_array(img)
-        x = preprocess_input(x)
+        x = resnet50.preprocess_input(x)
         X_test.append(x)
     print 'Total val examples: ' + str(len(y_test))
     print 'No hate examples: ' + str(total_no_hate)
@@ -115,6 +117,106 @@ def get_test_data_resnet50():
     X_test = np.array(X_test)
     y_test = np.array(y_test)
     return X_test, y_test
+
+'''
+********************************************************
+*******************XCEPTION*****************************
+********************************************************
+'''
+def build_xception_model():
+    # get the model without the denses
+    base_model = xception.Xception(weights='imagenet', include_top='false')
+    new_dense = base_model.output
+    # add the new denses to classify the hate images
+    new_dense = Dense(1024, activation='relu')(new_dense)
+    predictions = Dense(2, activation='softmax')(new_dense)
+    model = Model(inputs=base_model.input, outputs=predictions)
+    # we will only train the new denses for the baseline
+    for layer in base_model.layers:
+        layer.trainable = False
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics = ["accuracy"])
+    return model
+
+def get_training_data_xception():    
+    filenames = os.listdir(TRAIN_PATH) 
+    shuffle(filenames)
+    X_train = []
+    y_train = []
+    total_hate = 0
+    total_no_hate = 0
+    print '*****TRAIN*****'
+    for filename in filenames:
+        if "no_hate" in filename:
+            y_train.append([1,0])
+            total_no_hate += 1
+        else:
+            y_train.append([0,1])
+            total_hate += 1
+        img = image.load_img(TRAIN_PATH+filename, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = resnet50.preprocess_input(x)
+        X_train.append(x)
+    print 'Total train examples: ' + str(len(y_train))
+    print 'No hate examples: ' + str(total_no_hate)
+    print 'Hate examples: ' + str(total_hate)
+        
+    print '*****VAL*****'
+    filenames = os.listdir(VAL_PATH) 
+    shuffle(filenames)
+    X_val = []
+    y_val = []
+    total_hate = 0
+    total_no_hate = 0
+    for filename in filenames:
+        if "no_hate" in filename:
+            y_val.append([1,0])
+            total_no_hate += 1
+        else:
+            y_val.append([0,1])
+            total_hate += 1
+        img = image.load_img(VAL_PATH+filename, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = xception.preprocess_input(x)
+        X_val.append(x)
+    print 'Total val examples: ' + str(len(y_val))
+    print 'No hate examples: ' + str(total_no_hate)
+    print 'Hate examples: ' + str(total_hate)  
+    
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    X_val = np.array(X_val)
+    y_val = np.array(y_val)
+    return X_train, y_train, X_val, y_val
+
+def get_test_data_xception(): 
+    print '*****TEST*****'
+    filenames = os.listdir(TEST_PATH) 
+    shuffle(filenames)
+    X_test = []
+    y_test = []
+    total_hate = 0
+    total_no_hate = 0
+    for filename in filenames:
+        if "no_hate" in filename:
+            y_test.append(0)
+            total_no_hate += 1
+        else:
+            y_test.append(1)
+            total_hate += 1
+        img = image.load_img(TEST_PATH+filename, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = xception.preprocess_input(x)
+        X_test.append(x)
+    print 'Total val examples: ' + str(len(y_test))
+    print 'No hate examples: ' + str(total_no_hate)
+    print 'Hate examples: ' + str(total_hate)  
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+    return X_test, y_test
+
+
+
+
 
 def calculate_evaluation_metrics(y_gt, y_preds):        
     metric_types =  ['micro', 'macro', 'weighted']
@@ -137,7 +239,7 @@ if __name__ == "__main__":
     print 'Starting'
     print 'Building model...'
     sys.stdout.flush()
-    model = build_resnet50_model()
+    model = build_xception_model()
     model.summary()
     print 'Building data...'
     X_train, y_train, X_val, y_val = get_training_data_resnet50()
